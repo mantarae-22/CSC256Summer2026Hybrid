ce15dad45ef30085edeb131ec9a71bb960c8947c
Rae Crusoe <113061355+mantarae-22@users.noreply.github.com>
2026-07-08 10:31:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ // set the name by using JavaScript code
+document.getElementById("divName").textContent = "Rae";
+
+// set up a variable shortcut/nickname for the HTML element
+let divMajor = document.getElementById("divMajor");
+divMajor.textContent = "Surfing";
+
+// set the email address to anything you like
+document.getElementById("divEmail").textContent = "me@me.com";
+
+// set the graduation date 
+document.getElementById("divGradDate").textContent = "May 2035";
+
+
+// let's pretend we had some calculations and other code
+//divName.textContent = "updated value";
